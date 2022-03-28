1571e8262868438b80b399862d0ac572b62ee9d6
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2022-03-28 17:09:41 +0530

    clean prestart.sh

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/node-app
-sudo apt-get update
-sudo apt-get install nodejs npm -y
-sudo npm install #to be uncomented if buildspec.yml config not worked.
-sudo npm install pm2 -g
+
