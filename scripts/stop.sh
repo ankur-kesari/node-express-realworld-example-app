2f1d6b469f5e658c0fe494c0d144b9b94fa666c0
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2022-03-28 21:57:35 +0530

    update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+#changing the directory
+cd /home/ubuntu/node-app
+#getting the status of process in variable process_status
+process_status=$(sudo pm2 list | grep 'app' | awk '{print$18}')
+
+if [ $process_status == 'online' ]
+then
+        sudo pm2 stop /home/ubuntu/node-app/app.js
+        echo "process stoped successfully"
+else
+        echo "no process was running"
+fi
