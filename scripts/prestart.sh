3dbf9fa5fc4789f23f7049308e941923451b8873
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2022-08-04 14:32:13 +0530

    Update prestart.sh

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#aws-code-deploy-agent-installation-for-us-west-2
-echo "aws-code-deploy-agent-installation-for-us-west-2"
-sudo apt update -y
-sudo apt install ruby-full
-sudo apt install wget
-wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
-sudo service codedeploy-agent start
-#---------------------------------------------------------
 # nodejs-npm-pm2-installation
 echo "nodejs-npm-pm2-installation"
 sudo apt install nodejs npm -y
