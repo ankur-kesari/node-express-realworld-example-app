2fdb2377fb63f0a2e9fee1025216024e2b768b06
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2022-08-18 16:34:15 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,12 +4,5 @@ sudo pm2 start app.js
 sudo pm2 startup 
 sudo pm2 save
 
-sudo apt install -y awscli
-export AWS_DEFAULT_REGION=us-east-1
-
-#attach role AmazonSSMReadOnlyaccess
-
-export NODE_ENV=$(aws ssm get-parameter --name "NODE_ENV" --with-decrypt --query "Parameter.Value" --output text)
-
-export SHOTSTACK_KEY=$(aws ssm get-parameter --name "SHOTSTACK_KEY" --with-decrypt --query "Parameter.Value" --output text)
+sh "source ./test.sh"
 
